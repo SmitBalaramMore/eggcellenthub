42e87ad06af313daa253dfa78e2f1541471c1bac
SmitBalaramMore <smitbalarammore@gmail.com>
2024-02-05 17:54:47 +0530

    profile setting and profile image

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User,auth
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponse
-from .models import Profile
+from .models import Profile,Post
 
 
 
@@ -11,7 +11,14 @@ from .models import Profile
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request,'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html',{'user_profile': user_profile })
+
+
+@login_required(login_url='signin')
+def upload(request):
+    return HttpResponse('<h1>Upload View</h1>')
 
 @login_required(login_url='signin')
 def settings(request):
